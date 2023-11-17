63f53e7bceceee0d0619cd1f2d6e3e0d3a42a452
Yan921227 <125242772+Yan921227@users.noreply.github.com>
2023-11-17 13:31:20 +0800

    myDocumentViewer 與上面圖示

---
+++ WpfApp4/MyDocumentViewer.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// MyDocumentViewer.xaml 的互動邏輯
+    /// </summary>
+    public partial class MyDocumentViewer : Window
+    {
+        public MyDocumentViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
